0e5a18f60b7817c98ebfc531278a7773060728d2
Ilya Kuznetsov <ikyz9.work@gmail.com>
2022-11-26 22:17:28 +0400

    feat:add second solution "Stop gninnipS My sdroW!"

--- src/main/java/codewars/CodewarsSolution.java
+++ src/main/java/codewars/CodewarsSolution.java
@@ -1,21 +1,15 @@
 package codewars;
 
-import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CodewarsSolution {
 
     //6 kyu
     //Stop gninnipS My sdroW!
     public String spinWords(String sentence){
-        StringBuilder newString = new StringBuilder();
-        for (String word : Arrays.asList(sentence.split(" "))){
-            if (word.length() > 5){
-                StringBuilder stringBuilder = new StringBuilder(word);
-                stringBuilder.reverse();
-                word = stringBuilder.toString();
-            }
-            newString.append(word + " ");
-        }
-        return newString.substring(0, newString.length()-1);
+        return Stream.of(sentence.split(" "))
+                .map(word -> word.length() >=5 ? new StringBuilder(word).reverse().toString() : word)
+                .collect(Collectors.joining(" "));
     }
 }
