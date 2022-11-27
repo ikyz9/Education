800d5ee9cc5afb75a102c6489911dd2a1eae3ec5
Ilya Kuznetsov <ikyz9.work@gmail.com>
2022-11-27 14:27:26 +0400

    feat:add first solution "Mean Square Error"

--- src/main/java/codewars/CodewarsSolution.java
+++ src/main/java/codewars/CodewarsSolution.java
@@ -1,14 +1,14 @@
 package codewars;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+//import java.util.Arrays;
+//import java.util.stream.Collectors;
+//import java.util.stream.Stream;
 
 public class CodewarsSolution {
 
     //6 kyu
     //Stop gninnipS My sdroW!
-    public String spinWords(String sentence){
+    public static String spinWords(String sentence){
         return Stream.of(sentence.split(" "))
                 .map(word -> word.length() >=5 ? new StringBuilder(word).reverse().toString() : word)
                 .collect(Collectors.joining(" "));
@@ -33,4 +33,15 @@ public class CodewarsSolution {
         }
         return sum % n == 0 ? sum / n : -1;
     }
+
+    //5 kyu
+    //Mean Square Error
+    public static double averageArraySquares(int[] a, int[] b){
+        double sum =0;
+        for (int i =0; i< a.length; i++) {
+            sum += Math.pow(a[i] - b[i], 2);
+        }
+        return sum / a.length;
+    }
+
 }
--- src/test/java/CodewarsSolutionTest.java
+++ src/test/java/CodewarsSolutionTest.java
@@ -6,30 +6,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CodewarsSolutionTest {
 
-    CodewarsSolution codewarsSolution = new CodewarsSolution();
-
     @Test
     public void spinWordsTest() {
         String sentence = "Hey fellow warriors";
         String expect = "Hey wollef sroirraw";
-        assertEquals(expect, codewarsSolution.spinWords(sentence));
+        assertEquals(expect, CodewarsSolution.spinWords(sentence));
 
     }
 
     @Test
     public void sampleTests() {
-        assertArrayEquals(new int[] {2}, codewarsSolution.arrayDiff(new int [] {1,2}, new int[] {1}));
-        assertArrayEquals(new int[] {2,2}, codewarsSolution.arrayDiff(new int [] {1,2,2}, new int[] {1}));
-        assertArrayEquals(new int[] {1}, codewarsSolution.arrayDiff(new int [] {1,2,2}, new int[] {2}));
-        assertArrayEquals(new int[] {1,2,2}, codewarsSolution.arrayDiff(new int [] {1,2,2}, new int[] {}));
-        assertArrayEquals(new int[] {}, codewarsSolution.arrayDiff(new int [] {}, new int[] {1,2}));
+        assertArrayEquals(new int[] {2}, CodewarsSolution.arrayDiff(new int [] {1,2}, new int[] {1}));
+        assertArrayEquals(new int[] {2,2}, CodewarsSolution.arrayDiff(new int [] {1,2,2}, new int[] {1}));
+        assertArrayEquals(new int[] {1}, CodewarsSolution.arrayDiff(new int [] {1,2,2}, new int[] {2}));
+        assertArrayEquals(new int[] {1,2,2}, CodewarsSolution.arrayDiff(new int [] {1,2,2}, new int[] {}));
+        assertArrayEquals(new int[] {}, CodewarsSolution.arrayDiff(new int [] {}, new int[] {1,2}));
     }
 
     @Test
-    public void Test1() {
-        assertEquals(1, codewarsSolution.digPow(89, 1));
-        assertEquals(-1, codewarsSolution.digPow(92, 1));
-        assertEquals(51, codewarsSolution.digPow(46288, 3));
+    public void digPowTest() {
+        assertEquals(1, CodewarsSolution.digPow(89, 1));
+        assertEquals(-1, CodewarsSolution.digPow(92, 1));
+        assertEquals(51, CodewarsSolution.digPow(46288, 3));
 
     }
+
+    @Test
+    public void averageArraySquaresTest() {
+        int[] a1 = {1, 2, 3};
+        int[] b1 = {4, 5, 6};
+        assertEquals(9.0, CodewarsSolution.averageArraySquares(a1, b1), 1e-15);
+        int[] a2 = {10, 20, 10, 2};
+        int[] b2 = {10, 25, 5, -2};
+        assertEquals(16.5, CodewarsSolution.averageArraySquares(a2, b2), 1e-15);
+        int[] a3 = {0, -1};
+        int[] b3 = {-1, 0};
+        assertEquals(1.0, CodewarsSolution.averageArraySquares(a3, b3), 1e-15);
+        int[] a4 = {10, 10};
+        int[] b4 = {10, 10};
+        assertEquals(0.0, CodewarsSolution.averageArraySquares(a4, b4), 1e-15);
+    }
+
+
 }
