554fe03f51ff7656f48fa1f52528e371a2eb5844
Ilya Kuznetsov <ikyz9.work@gmail.com>
2023-01-18 15:47:16 +0400

    feat:add second solution "Maximum subarray sum"

--- src/main/java/codewars/kyu5/Max.java
+++ src/main/java/codewars/kyu5/Max.java
@@ -8,17 +8,15 @@ public class Max {
 
     //new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4})
     public static int sequence(int[] arr) {
-        if (arr.length == 0) { return 0; }
-
         int maxSum = 0, currentSum = 0;
 
-        for (int i = 0; i < arr.length; i++){
-            currentSum += arr[i];
+        for (int j : arr) {
+            currentSum += j;
             if (currentSum > maxSum) { maxSum = currentSum; }
-            if (currentSum < 0){ currentSum = 0; }
+            if (currentSum < 0) { currentSum = 0; }
         }
 
-        return maxSum != 0 ? maxSum : 0;
+        return maxSum;
     }
 }
 
--- src/test/java/CodewarsSolutionTest.java
+++ src/test/java/CodewarsSolutionTest.java
@@ -121,4 +121,13 @@ public class CodewarsSolutionTest {
                 , ProdFib.productFib(5895));
     }
 
+    @Test
+    public void testEmptyArray(){
+        assertEquals(0, Max.sequence(new int[]{}));
+    }
+    @Test
+    public void testExampleArray(){
+        assertEquals(6, Max.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
 }
